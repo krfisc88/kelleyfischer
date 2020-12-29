d5a0c96b9801b8a530fa9b318ad6e1e5588c4f0a
Kelley Fischer <krfisc@outlook.com>
2020-12-29 14:23:20 -0600

    modify scroll bar and layout of homepage

--- shared/scripts/shared.js
+++ shared/scripts/shared.js
@@ -1,15 +1,19 @@
 // Handle Overlay Menu for mobile devices
+
 const menu = document.getElementById('toggle');
-const navbar = document.getElementById('navbar');
+
+function mobileMenu() {
+    const navbar = document.getElementById('navbar');
+    const body = document.querySelector('body');
+    const menuIcon = document.querySelector('#toggle i')
+
+    navbar.classList.toggle('collapse');
+    menu.classList.toggle('js-mobile-menu');
+    body.classList.toggle('js-mobile-menu-open');
+    menuIcon.classList.toggle('fa-times');
+    menuIcon.classList.toggle('fa-bars');
+}
 
 menu.addEventListener('click', () => {
-    if(navbar.classList.contains('collapse')) {
-        navbar.classList.remove('collapse');
-        menu.innerHTML = '<i class="fas fa-times fa-2x"></i>';
-        menu.classList.add('mobile-menu');
-    } else {
-        navbar.classList.add('collapse');
-        menu.innerHTML = '<i class="fas fa-bars fa-2x">';
-        menu.classList.remove('mobile-menu');
-    }
+    mobileMenu();
 });
